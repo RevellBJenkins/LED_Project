aa6b5a2197cfd006992116e91bd0ff0a44a71923
Revell B Jenkins <revell.jenkins.dev@gmail.com>
2025-09-27 21:47:48 -0600

    Add files via upload

---
+++ LED_Project_One.ino
@@ -0,0 +1,124 @@
+// LED Pins
+const int led1 = 13;
+const int led2 = 12;
+const int led3 = 11;
+const int led4 = 10;
+const int led5 = 9;
+const int led6 = 8;
+const int led7 = 7;
+const int led8 = 6;
+const int led9 = 5;
+const int led10 = 4;
+
+
+void setup() {
+  // Set pins as output
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
+  pinMode(led5, OUTPUT);
+  pinMode(led6, OUTPUT);
+  pinMode(led7, OUTPUT);
+  pinMode(led8, OUTPUT);
+  pinMode(led9, OUTPUT);
+  pinMode(led10, OUTPUT);
+}
+
+void loop() {
+  // Turn on LED 1
+  digitalWrite(led1, HIGH);
+  delay(500);
+  digitalWrite(led1, LOW);
+
+  // Turn on LED 2
+  digitalWrite(led2, HIGH);
+  delay(100);
+  digitalWrite(led2, LOW);
+
+  // Turn on LED 3
+  digitalWrite(led3, HIGH);
+  delay(100);
+  digitalWrite(led3, LOW);
+
+    // Turn on LED 4
+  digitalWrite(led4, HIGH);
+  delay(100);
+  digitalWrite(led4, LOW);
+
+  // Turn on LED 5
+  digitalWrite(led5, HIGH);
+  delay(100);
+  digitalWrite(led5, LOW);
+
+  // Turn on LED 6
+  digitalWrite(led6, HIGH);
+  delay(100);
+  digitalWrite(led6, LOW);
+
+    // Turn on LED 7
+  digitalWrite(led7, HIGH);
+  delay(100);
+  digitalWrite(led7, LOW);
+
+  // Turn on LED 8 
+  digitalWrite(led8, HIGH);
+  delay(100);
+  digitalWrite(led8, LOW);
+
+  // Turn on LED 9
+  digitalWrite(led9, HIGH);
+  delay(100);
+  digitalWrite(led9, LOW);
+
+    // Turn on LED 10
+  digitalWrite(led10, HIGH);
+  delay(500);
+  digitalWrite(led10, LOW);
+
+  // Turn on LED 9
+  digitalWrite(led9, HIGH);
+  delay(100);
+  digitalWrite(led9, LOW);
+
+  // Turn on LED 7
+  digitalWrite(led7, HIGH);
+  delay(100);
+  digitalWrite(led7, LOW);
+
+  // Turn on LED 5
+  digitalWrite(led5, HIGH);
+  delay(100);
+  digitalWrite(led5, LOW);
+
+  // Turn on LED 3
+  digitalWrite(led3, HIGH);
+  delay(100);
+  digitalWrite(led3, LOW);
+
+  // Turn on LED 2
+  digitalWrite(led2, HIGH);
+  delay(100);
+  digitalWrite(led2, LOW);
+
+  // Turn on LED 4
+  digitalWrite(led4, HIGH);
+  delay(100);
+  digitalWrite(led4, LOW);
+
+  // Turn on LED 6
+  digitalWrite(led6, HIGH);
+  delay(100);
+  digitalWrite(led6, LOW);
+
+  // Turn on LED 8
+  digitalWrite(led8, HIGH);
+  delay(100);
+  digitalWrite(led8, LOW);
+
+  // Turn on LED 10
+  digitalWrite(led10, HIGH);
+  delay(100);
+  digitalWrite(led10, LOW);
+
+}
